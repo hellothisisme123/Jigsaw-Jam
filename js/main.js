d2347df584bf4ef6e4622b29a40de26751d5f5f9
Ashley Lawton <115205055+hellothisisme123@users.noreply.github.com>
2025-01-16 09:09:41 -0500

    navbar

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
+// ----- get db connection -----
+let database
 fetch('http://192.168.240.9:3006/jigsawJam/data')
     .then(response => response.json())
     .then(data => {
         console.log(data);
+        database = data
     })
     .catch(error => {
         console.error('Error fetching data:', error);
     });
+
+// ----- nav -----
+const homeBtn = document.querySelector("nav .home")
+const settingsBtn = document.querySelector("nav .settings")
+homeBtn.addEventListener("click", () => {window.location = "./index.html"})
+settingsBtn.addEventListener("click", () => {window.location = "./settings.html"})
