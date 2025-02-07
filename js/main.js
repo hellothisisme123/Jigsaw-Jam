595315892014a1401c1aa2ca8e377ecd88330231
Ashley Lawton <115205055+hellothisisme123@users.noreply.github.com>
2025-02-07 12:26:07 -0500

    started home page, did responsiveness on signin and such, created alert popup

--- js/main.js
+++ js/main.js
@@ -21,3 +21,56 @@ const requireJavascript = document.querySelectorAll(".requireJavascript")
 requireJavascript.forEach(el => {
     el.classList.remove("requireJavascript") 
 })
+
+// ----- alert popup -----
+// title   | the title text
+// text    | the main text
+// yesText | the text  in the yes button
+// noText  | the text in the no button
+// yesFunc | a function that runs when the user presses yes
+// noFunc  | a function that runs when the user presses no
+function alertPopup(title, text, yesText, noText, yesFunc, noFunc) {
+    let html = 
+    `<div class="alertPopup">
+        <div class="bgWrapper">
+            <div class="responsive">
+                <div class="title">${title}</div>
+                <div class="text">
+                    ${text} 
+                </div>
+                <div class="buttonWrapper">
+                    <div class="yes">
+                        ${yesText}
+                    </div>
+                    <div class="no">
+                        ${noText}                    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>`
+    const container = document.querySelector(".container")
+    container.innerHTML += html
+    const alertPopup = container.querySelector(".alertPopup")
+    const yesBtn = alertPopup.querySelector(".yes")
+    const noBtn = alertPopup.querySelector(".no")
+    console.log(alertPopup);
+
+    yesBtn.addEventListener("click", (e) => {
+        yesFunc()
+        alertPopup.remove()
+    })
+    noBtn.addEventListener("click", (e) => {
+        noFunc()
+        alertPopup.remove()
+    })
+}
+
+// alertPopup(
+//     "Are you Sure?",
+//     "You would like to permanently delete your account. Other users will be able to create a new account using the same username. All settings, saved puzzles, and saved data will be deleted. The process is non-reversible.",
+//     "Yes, Delete Account",
+//     "No, Cancel",
+//     () => {console.log("yes")},
+//     () => {console.log("no")}
+// )
